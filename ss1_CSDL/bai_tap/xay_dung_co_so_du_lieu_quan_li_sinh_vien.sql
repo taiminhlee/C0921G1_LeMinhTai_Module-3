571438923e7d2aec2bea851df11899e56c11aad3
taiminhlee <pck14451@gmail.com>
2021-12-08 17:06:58 +0700

    upnai

---
+++ ss1_CSDL/bai_tap/xay_dung_co_so_du_lieu_quan_li_sinh_vien.sql
@@ -0,0 +1,10 @@
+CREATE TABLE `class` (
+`id` INT NOT NULL,
+`name` VARCHAR(45) NULL,
+PRIMARY KEY (id));
+CREATE TABLE `teacher` (
+`id` INT NOT NULL,
+`name` VARCHAR(45) NULL,
+`age` INT NULL,
+`country` VARCHAR(45) NULL,
+PRIMARY KEY (id));
