1888e6cad4dd71934cd914491ec143a97f072ad7
taiminhlee <pck14451@gmail.com>
2021-12-09 20:36:42 +0700

    upbai

--- ss1_CSDL/bai_tap/xay_dung_co_so_du_lieu_quan_li_sinh_vien.sql
+++ ss1_CSDL/bai_tap/xay_dung_co_so_du_lieu_quan_li_sinh_vien.sql
@@ -2,6 +2,7 @@ CREATE TABLE `class` (
 `id` INT NOT NULL,
 `name` VARCHAR(45) NULL,
 PRIMARY KEY (id));
+
 CREATE TABLE `teacher` (
 `id` INT NOT NULL,
 `name` VARCHAR(45) NULL,
--- ss1_CSDL/thuc_hanh/tao_bang_tren_MyQSL_Workbrench.sql
+++ ss1_CSDL/thuc_hanh/tao_bang_tren_MyQSL_Workbrench.sql
@@ -1,6 +1,15 @@
 CREATE TABLE `student` (
-`id` INT NOT NULL,
-`name` VARCHAR(45) NULL,
-`age` INT NULL,
-`country` VARCHAR(45) NULL,
-PRIMARY KEY (id));
+`id` INT PRIMARY KEY,
+`name` VARCHAR(45),
+`age` INT,
+`country` VARCHAR(45)
+ );
+
+insert into `student`
+value 
+(1, 'Nguyễn Văn A', 20, 'Việt Nam'),
+(2, 'Nguyễn Văn B', 21, 'Việt Nam');
+
+update student
+set country = 'Thái Lan'
+where id=2;
